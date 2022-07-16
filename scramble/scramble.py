c26b7138d4a6fe4a595a2751a4b8254b279dcb73
Andreas Papaconstantinou <IPapaconstantinou@fxcess.com>
2022-07-16 11:41:35 +0000

    restructure

---
+++ scramble/scramble.py
@@ -0,0 +1,30 @@
+import itertools
+import pprint
+
+print("running...")
+# Todo: __init__: for each dictionary append to the list its scrambled version
+
+class Scramble:
+	def isMatch(dictionary: list, longString: str)-> bool:
+		if longString in dictionary:
+			return True
+		return False
+
+	def generateScrambles(dictionary: str)-> list:
+		scrambles = []
+		permutations = list(map("".join, itertools.permutations(dictionary)))
+		for p in permutations:
+				if (p[0] == dictionary[0] and p[-1] == dictionary[-1]):
+					scrambles.append(p)
+		return scrambles
+
+
+# dictionary_list = ['axpaj', 'apxaj', 'dnrbt', 'pjxdn', 'abd']
+
+# # r = isScramble(dictionary_list, 'aapxjdnrbtvldptfzbbdbbzxtndrvjblnzjfpvhdhhpxjdnrbt')
+
+# # print(r)
+# Scramble = Scramble()
+
+# print(Scramble.generateScrambles('this'))
+# print("Completed")
