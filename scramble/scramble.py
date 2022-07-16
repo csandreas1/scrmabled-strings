2facd5d10e0b8455efba936db2ef871eecc6d049
Andreas Papaconstantinou <IPapaconstantinou@fxcess.com>
2022-07-16 13:29:17 +0000

    generation of scrambles and orchesration of data in regards to test driven development

--- scramble/scramble.py
+++ scramble/scramble.py
@@ -5,12 +5,26 @@ print("running...")
 # Todo: __init__: for each dictionary append to the list its scrambled version
 
 class Scramble:
-	def isMatch(dictionary: list, longString: str)-> bool:
-		if longString in dictionary:
-			return True
-		return False
 
-	def generateScrambles(dictionary: str)-> list:
+	def __init__(self, dictionary: list) -> None:
+		self.dictionary_list = dictionary
+		self.scrambles = []
+		self.matches = 0
+
+
+		for d in self.dictionary_list:
+			self.scrambles.extend(self.generateScrambles(d))
+
+		self.orchistrate_generated_scrambles()
+		pprint.pprint(self.dictionary_list)
+
+	def get_matches(self, longString: str)-> int:
+		for d in self.dictionary_list:
+			if d in longString:
+				self.matches = self.matches + 1
+		return self.matches
+
+	def generateScrambles(self, dictionary: str)-> list:
 		scrambles = []
 		permutations = list(map("".join, itertools.permutations(dictionary)))
 		for p in permutations:
@@ -18,6 +32,11 @@ class Scramble:
 					scrambles.append(p)
 		return scrambles
 
+	def orchistrate_generated_scrambles(self)-> None:
+		#remove duplicates
+		self.scrambles = list(dict.fromkeys(self.scrambles))
+		#merge scrumble list into the dictionary list
+		self.dictionary_list = itertools.chain(self.dictionary_list, self.scrambles)
 
 # dictionary_list = ['axpaj', 'apxaj', 'dnrbt', 'pjxdn', 'abd']
 
