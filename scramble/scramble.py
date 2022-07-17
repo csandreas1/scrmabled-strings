5996f523040e7fb297676e4cbd1f3d6776a0b21f
Andreas Papaconstantinou <IPapaconstantinou@fxcess.com>
2022-07-17 18:33:43 +0000

    logic update

--- scramble/scramble.py
+++ scramble/scramble.py
@@ -5,58 +5,27 @@ import logging.config
 class Scramble:
 	def __init__(self, dictionary: list = [])-> None:
 		self.dictionary_list = dictionary
-		self.scrambles = []
-		self.dictionary_with_scrambles = []
 		self.matches = 0
+		self.anagrams = {}
+		self.scrambles = []
 		logging.info("Dictionary List:")
 		logging.debug(self.dictionary_list)
 
-		for d in self.dictionary_list:
-			self.scrambles.extend(self.generateScrambles(d))
 
-		logging.info("Generated scrambles:")
-		logging.debug(self.scrambles)
+	def is_anagram(self, str1, str2):
+		lettercount = len(str1)
+		if lettercount not in self.anagrams:
+			# build the list for that letter length
+			self.anagrams[lettercount] = [sorted(str2[x:x+lettercount]) for x in range(len(str2)-lettercount)]
+		return (sorted(str1) in self.anagrams[lettercount])
 
-		self.orchistrate_generated_scrambles()
-
-		logging.info("Dictionary list with scrambles: ")
-		logging.debug(self.dictionary_with_scrambles)
-
-	def get_matches(self, longString: str)-> int:
+	def get_matches(self, long_string: str)-> int:
 		self.matches = 0
-		matches = []
-		dictionary_scrambles_advanced = list(map(self.get_advanced_data, self.dictionary_with_scrambles))
-
-		for d in self.dictionary_with_scrambles:
-			if d in longString:
+		for l in self.dictionary_list:
+			if self.is_anagram(l, long_string):
+				self.scrambles += [l]
 				self.matches += 1
-				matches.append(d)
-				#advanced matching checks
-				for data in dictionary_scrambles_advanced:
-					if data[0] not in matches \
-							and (d[0] + d[-1] == data[1] and len(d) == data[2]):
-						self.matches += 1
-						matches.append(data[0])
 
-		logging.info(f"{self.matches} matches found for input '{longString}'")
-		logging.debug(matches)
+		logging.info(f"{self.matches} matches found for input '{long_string}'")
+		logging.debug(self.scrambles)
 		return self.matches
-
-	def generateScrambles(self, dictionary: str)-> list:
-		scrambles = []
-		permutations = list(map("".join, itertools.permutations(dictionary)))
-		for p in permutations:
-				if p not in self.dictionary_list and (
-					p[0] == dictionary[0] and p[-1] == dictionary[-1]
-				):
-					scrambles.append(p)
-		return scrambles
-
-	def orchistrate_generated_scrambles(self)-> None:
-		#remove duplicates
-		self.scrambles = list(dict.fromkeys(self.scrambles))
-		#merge scrumble list into the dictionary list
-		self.dictionary_with_scrambles = list(itertools.chain(self.dictionary_list, self.scrambles))
-
-	def get_advanced_data(self, string:str)-> list:
-		return [string, string[0] + string[-1], len(string)]
--- scramble/test_scramble.py
+++ scramble/test_scramble.py
@@ -2,24 +2,17 @@ import unittest
 from scramble import Scramble
 
 class TestingScramble(unittest.TestCase):
-    def test_generate_scrambles(self):
-        scramble = Scramble()
-        scrambled_list = scramble.generateScrambles("this")
-        self.assertEqual(scrambled_list, ["this", "tihs"])
-
-    def test_generate_scrambles_2(self):
-        scramble = Scramble()
-        scrambled_list = scramble.generateScrambles("pool")
-        self.assertEqual(scrambled_list, ["pool", "pool"])
-
-    def test_dictionary_merged_with_scrambles(self):
-        scramble = Scramble(["this"])
-        self.assertEqual(scramble.dictionary_list + scramble.scrambles, ["this", "tihs"])
 
     def test_get_matches(self):
         scramble = Scramble(["axpaj", "apxaj", "dnrbt", "pjxdn", "abd"])
         matches = scramble.get_matches("aapxjdnrbtvldptfzbbdbbzxtndrvjblnzjfpvhdhhpxjdnrbt")
-        self.assertEqual(matches, 18)
+        self.assertEqual(matches, 4)
+
+    def test_get_result(self):
+        scramble = Scramble(["axpaj", "apxaj", "dnrbt", "pjxdn", "abd", "whatever"])
+        scramble.get_matches("aapxjdnrbtvldptfzbbdbbzxtndrvjblnzjfpvhdhhpxjdnrbt")
+
+        self.assertEqual(scramble.scrambles, ["axpaj", "apxaj", "dnrbt", "pjxdn"])
 
 if __name__ == "__main__":
     unittest.main()
